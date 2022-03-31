625a9dd27c887d374f3037b929759067c2e83711
Rui Wu <ruiwu@Ruis-MacBook-Air.local>
2022-03-31 14:59:19 -0400

    UI completed for main activity

--- UIDesign_Project/app/src/main/java/com/example/uidesign_project/MainActivity.java
+++ UIDesign_Project/app/src/main/java/com/example/uidesign_project/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.uidesign_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createAccountButton = findViewById(R.id.btn_create_account);
+        createAccountButton.setOnClickListener(view -> {
+           Intent createAccountIntent = new Intent(MainActivity.this, CreateAccountActivity.class);
+
+           startActivity(createAccountIntent);
+
+        });
     }
 }
