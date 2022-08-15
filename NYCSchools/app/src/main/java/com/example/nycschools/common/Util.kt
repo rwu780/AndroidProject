28868ddac51b00fb9e48a7e748353f8768d9c28a
James <rwu780@gmail.com>
2022-08-14 20:02:05 -0700

    Delete NYCSchools directory

--- NYCSchools/app/src/main/java/com/example/nycschools/common/Util.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nycschools.common
-
-const val BASE_URL = "https://data.cityofnewyork.us/"
-const val END_POINT_SCHOOLS = "resource/s3k6-pzi2.json"
-const val END_POINT_SAT = "resource/f9bf-2cp4.json"
-
